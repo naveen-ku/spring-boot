c6b315c5e1b1247e95540627f23f504e48835ab3
Naveen Kumar <naveen2951996@gmail.com>
2025-09-20 22:43:28 +0530

    feat: use setter DI for rest controller

---
+++ src/main/java/com/naveen/springboot/demo/springbootdemo/controller/DemoRestControllerSetterInjection.java
@@ -0,0 +1,23 @@
+package com.naveen.springboot.demo.springbootdemo.controller;
+
+import com.naveen.springboot.demo.springbootdemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestControllerSetterInjection {
+
+    private Coach myCoach;
+
+    @Autowired
+    public void setCoach(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/setter-dailyworkout")
+    public String route(){
+        return "Setter: " + myCoach.getDailyWorkout();
+    }
+
+}
