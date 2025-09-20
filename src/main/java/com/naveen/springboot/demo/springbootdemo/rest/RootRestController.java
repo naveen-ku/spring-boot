d5fdc37225bc70650b2b8fcca8162532bf92cd54
Naveen Kumar <naveen2951996@gmail.com>
2025-09-20 11:43:44 +0530

    feat: add devtools for hot reload/ server restart on live changes

--- src/main/java/com/naveen/springboot/demo/springbootdemo/rest/RootRestController.java
+++ src/main/java/com/naveen/springboot/demo/springbootdemo/rest/RootRestController.java
@@ -1,14 +1,27 @@
 package com.naveen.springboot.demo.springbootdemo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class RootRestController {
 
     // expose "/" endpoint
     @GetMapping("/")
+    public String rootRoute(){
+        return "Root world live changes";
+    }
+
+    @GetMapping("/hello")
     public  String sayHello(){
         return "Hello world!";
     }
+
+    @GetMapping("/new")
+    public String newEnd(){
+        return  "New";
+    }
+
 }
