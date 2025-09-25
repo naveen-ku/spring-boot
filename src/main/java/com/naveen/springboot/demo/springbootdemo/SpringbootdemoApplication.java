d6b5d9bb97e655468924c4398472eb7b315fe7f5
Naveen Kumar <naveen2951996@gmail.com>
2025-09-25 09:21:06 +0530

    feat: create Student entity to store in db

--- src/main/java/com/naveen/springboot/demo/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/naveen/springboot/demo/springbootdemo/SpringbootdemoApplication.java
@@ -1,5 +1,7 @@
 package com.naveen.springboot.demo.springbootdemo;
 
+import com.naveen.springboot.demo.springbootdemo.dao.StudentDAO;
+import com.naveen.springboot.demo.springbootdemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,21 @@ public class SpringbootdemoApplication {
 	}
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args){
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
         return runner ->{
-            System.out.println("Hello command line runner!");
+            createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+
+        System.out.println("Creating new student object ....");
+        Student tempStudent = new Student("Naveen", "Kumar", "naveen@gmail.com");
+
+        System.out.println("Saving the student");
+        studentDAO.save(tempStudent);
+
+        System.out.println("Saved student. Generated id: " + tempStudent.getId());
     }
 
 }
---
+++ src/main/java/com/naveen/springboot/demo/springbootdemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.naveen.springboot.demo.springbootdemo.dao;
+
+import com.naveen.springboot.demo.springbootdemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+}
