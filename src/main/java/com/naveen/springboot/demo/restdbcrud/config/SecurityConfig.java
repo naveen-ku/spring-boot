d778e6b09f05efa9628414c4abae6b296c6b9a46
Naveen Kumar <naveen2951996@gmail.com>
2025-09-28 22:09:49 +0530

    feat: security config to test apis in postman
    feat: PATCH api for employee partial update

---
+++ src/main/java/com/naveen/springboot/demo/restdbcrud/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.naveen.springboot.demo.restdbcrud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable()); // 🔑 disable CSRF for Postman testing
+        return http.build();
+    }
+
+}
--- src/main/java/com/naveen/springboot/demo/restdbcrud/controller/EmployeeController.java
+++ src/main/java/com/naveen/springboot/demo/restdbcrud/controller/EmployeeController.java
@@ -1,22 +1,27 @@
 package com.naveen.springboot.demo.restdbcrud.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.naveen.springboot.demo.restdbcrud.entity.Employee;
 import com.naveen.springboot.demo.restdbcrud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
 
     private EmployeeService mEmployeeService;
+    private ObjectMapper mObjectMapper;
 
     @Autowired
-    public void EmployeeController(EmployeeService employeeService){
+    public void EmployeeController(EmployeeService employeeService, ObjectMapper objectMapper){
         this.mEmployeeService = employeeService;
+        this.mObjectMapper = objectMapper;
     }
 
     @GetMapping("/employees")
@@ -26,12 +31,20 @@ public class EmployeeController {
 
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployeesById(@PathVariable int employeeId){
-        return mEmployeeService.findById(employeeId);
+        Employee employee =  mEmployeeService.findById(employeeId);
+        if(employee == null){
+            throw new RuntimeException("Employee not found - " + employeeId);
+        }
+        return employee;
     }
 
     @PostMapping("/employees")
-    public Employee saveEmployees(){
-        Employee employee = new Employee("Naveen","Kumar","naveen@gmail.com");
+    public Employee saveEmployees(@RequestBody Employee employee){
+
+        // Case if the user add some id value then we are setting it to 0 because in DAOImpl
+        // we used the merge function which create new record if id is not found (or 0) or
+        // update if id is found
+        employee.setId(0);
         Employee dbEmployee =  mEmployeeService.save(employee);
         return dbEmployee;
     }
@@ -45,6 +58,46 @@ public class EmployeeController {
     @DeleteMapping("/employees")
     public String deleteAllEmployees(){
         int numRowDeleted = mEmployeeService.deleteAll();
-        return "Number of rows deleted" + numRowDeleted;
+        return "Number of rows deleted - " + numRowDeleted;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        Employee dbEmployee = mEmployeeService.save(employee);
+        return dbEmployee;
+    }
+
+    @PatchMapping("/employees/{employeeId}")
+    public Employee partialUpdateEmployee(@PathVariable int employeeId, @RequestBody Map<String, Object> patchPayload){
+
+        Employee dbEmployee = mEmployeeService.findById(employeeId);
+
+        if(dbEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        if(patchPayload.containsKey("id")){
+            throw new RuntimeException("Employee id is not allowed in request body - " + employeeId);
+        }
+
+        Employee patchedEmployee = apply(patchPayload, dbEmployee);
+
+        Employee updatedEmployee = mEmployeeService.save(patchedEmployee);
+
+        return updatedEmployee;
+    }
+
+    private Employee apply(Map<String, Object> patchPayload, Employee dbEmployee){
+
+//        Convert employee object to a JSON object node
+        ObjectNode dbEmployeeNode = mObjectMapper.convertValue(dbEmployee, ObjectNode.class);
+
+//        Convert patchPayload map to a JSON object node
+        ObjectNode patchNode = mObjectMapper.convertValue(patchPayload, ObjectNode.class);
+
+//        Merge the patch updates into the employee node
+        dbEmployeeNode.setAll(patchNode);
+
+        return mObjectMapper.convertValue(dbEmployeeNode, Employee.class);
     }
 }
