a2a3fe61a205e79874120b8dd4ddb1d5fb31c7fd
Naveen Kumar <naveen2951996@gmail.com>
2025-09-30 21:18:17 +0530

    feat: add basic securiy config in memory

--- src/main/java/com/naveen/springboot/demo/restjpadatacrud/config/SecurityConfig.java
+++ src/main/java/com/naveen/springboot/demo/restjpadatacrud/config/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +20,35 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable()); // 🔑 disable CSRF for Postman testing
         return http.build();
+    }
+    
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}john")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}mary")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}susan")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        UserDetails admin = User.builder().
+                username("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan, admin);
     }
 
 }
