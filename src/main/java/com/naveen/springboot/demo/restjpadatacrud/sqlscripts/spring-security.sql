a6221753ca6c7423e37e0cd11f264e28c17853b4
Naveen Kumar <naveen2951996@gmail.com>
2025-10-02 20:07:46 +0530

    feat: use bcrypt hashed password in db

--- src/main/java/com/naveen/springboot/demo/restjpadatacrud/sqlscripts/spring-security.sql
+++ src/main/java/com/naveen/springboot/demo/restjpadatacrud/sqlscripts/spring-security.sql
@@ -7,17 +7,29 @@ DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
 `username` varchar(50) NOT NULL,
-`password` varchar(50) NOT NULL,
+`password` char(68) NOT NULL,
 `enabled` tinyint NOT NULL,
 PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Case 1 when stored unencrypted password in db
 INSERT INTO `users`
 VALUES
 ('john','{noop}test123',1),
 ('mary','{noop}test123',1),
 ('susan','{noop}test123',1),
 ('admin','{noop}admin',1);
+
+
+-- Case 2 when storing bcrypt hashed password in db
+
+INSERT INTO `users`
+VALUES
+('john','{bcrypt}$2a$12$1o.oPWDRmZyHEtbb0fTEQO4UdIHPlan85/38McZWR32IKS6dBhKmy',1),
+('mary','{bcrypt}$2a$12$PHsh4Ec7Nn9y0lAQxde35ufMkZ3Eu5o2vsLKY6hmQB33ThkRK0v5G',1),
+('susan','{bcrypt}$2a$12$vAnkc527FKMYyztiWodeCOZvKljwmjrYOyXCIEddp/ZUBrwqqnbom',1),
+('admin','{bcrypt}$2a$12$W6.CIc8w2AnUCxbjVP5aSe1YrC.PqKWWlSmM3ItBHgVI6AqsRCql2',1);
+
 
 CREATE TABLE `authorities` (
 `username` varchar(50) NOT NULL,
