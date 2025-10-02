9c558bfd651db7423e5b6090f264ff9976a04e51
Naveen Kumar <naveen2951996@gmail.com>
2025-10-02 20:30:57 +0530

    feat: spring boot security with custom tables

--- src/main/java/com/naveen/springboot/demo/restjpadatacrud/config/SecurityConfig.java
+++ src/main/java/com/naveen/springboot/demo/restjpadatacrud/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsChecker;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
@@ -29,9 +30,9 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.DELETE, "/api/employees").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET,"/swagger-ui/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET,"/v3/**").hasRole("ADMIN") //v3/api-docs
-                );
+                        .requestMatchers(HttpMethod.GET, "/swagger-ui/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/v3/**").hasRole("ADMIN") //v3/api-docs
+        );
 
 //        use HTTP Basic authentication
         http.httpBasic(Customizer.withDefaults());
@@ -69,8 +70,27 @@ public class SecurityConfig {
 //        return new InMemoryUserDetailsManager(john, mary, susan, admin);
 //    }
 
+//    Use this for when using default table names for spring boot security using DB
+//    @Bean
+//    public UserDetailsManager userDetailsManager(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+    //    Use this for custom name tables for spring boot security using DB
     @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+//        define a query to retrieve a user by username/user_id
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?");
+
+//        define a query to retrieve the authority/role by username/user_id
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
+
     }
 }
--- src/main/java/com/naveen/springboot/demo/restjpadatacrud/sqlscripts/spring-security-bcrypt-custom-table.sql
+++ src/main/java/com/naveen/springboot/demo/restjpadatacrud/sqlscripts/spring-security-bcrypt-custom-table.sql
@@ -1,30 +1,22 @@
 CREATE DATABASE  IF NOT EXISTS `employee_directory`;
 USE `employee_directory`;
 
--- Drop authorities first & then users because of foreign key reference
--- Spring boot by default use these two tables with exact same name for auto configuring the security
 DROP TABLE IF EXISTS `authorities`;
 DROP TABLE IF EXISTS `users`;
+-- Drop roles first & then members because of foreign key reference
+DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `members`;
 
-CREATE TABLE `users` (
-`username` varchar(50) NOT NULL,
-`password` char(68) NOT NULL,
-`enabled` tinyint NOT NULL,
-PRIMARY KEY (`username`)
+CREATE TABLE `members` (
+`user_id` varchar(50) NOT NULL,
+`pw` char(68) NOT NULL,
+`active` tinyint NOT NULL,
+PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Case 1 when stored unencrypted password in db
-INSERT INTO `users`
-VALUES
-('john','{noop}test123',1),
-('mary','{noop}test123',1),
-('susan','{noop}test123',1),
-('admin','{noop}admin',1);
-
 
 -- Case 2 when storing bcrypt hashed password in db
 
-INSERT INTO `users`
+INSERT INTO `members`
 VALUES
 ('john','{bcrypt}$2a$12$1o.oPWDRmZyHEtbb0fTEQO4UdIHPlan85/38McZWR32IKS6dBhKmy',1),
 ('mary','{bcrypt}$2a$12$PHsh4Ec7Nn9y0lAQxde35ufMkZ3Eu5o2vsLKY6hmQB33ThkRK0v5G',1),
@@ -32,16 +24,16 @@ VALUES
 ('admin','{bcrypt}$2a$12$W6.CIc8w2AnUCxbjVP5aSe1YrC.PqKWWlSmM3ItBHgVI6AqsRCql2',1);
 
 
-CREATE TABLE `authorities` (
-`username` varchar(50) NOT NULL,
-`authority` varchar(50) NOT NULL,
-UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
-CONSTRAINT `authorities_ibfk_1`
-FOREIGN KEY (`username`)
-REFERENCES `users` (`username`)
+CREATE TABLE `roles` (
+`user_id` varchar(50) NOT NULL,
+`role` varchar(50) NOT NULL,
+UNIQUE KEY `roles_idx_1` (`user_id`,`role`),
+CONSTRAINT `roles_ibfk_1`
+FOREIGN KEY (`user_id`)
+REFERENCES `members` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `authorities`
+INSERT INTO `roles`
 VALUES
 ('john','ROLE_EMPLOYEE'),
 ('mary','ROLE_EMPLOYEE'),
