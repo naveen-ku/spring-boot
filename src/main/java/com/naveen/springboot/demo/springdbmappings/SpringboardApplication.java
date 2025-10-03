e01d49a80ee7e55be819afda9f3c0388d47760ac
Naveen Kumar <naveen2951996@gmail.com>
2025-10-03 19:33:32 +0530

    init: starter code for hibernate mappings package

---
+++ src/main/java/com/naveen/springboot/demo/springdbmappings/SpringboardApplication.java
@@ -0,0 +1,12 @@
+package com.naveen.springboot.demo.springdbmappings;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboardApplication {
+    static void main(String[] args) {
+        SpringApplication.run(SpringboardApplication.class, args);
+    }
+}
